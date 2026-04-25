7d00c4f40d3d58266e61612ac717112c28f7ca11
ovo241089 <OVO241089@GMAIL.COM>
2026-04-25 17:19:08 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,20 +93,13 @@ export default function RootLayout({
         />
       </head>
       <body className={`${inter.className} gradient-bg`}>
-{/* Google tag (gtag.js) */}
-<Script
-  src="https://www.googletagmanager.com/gtag/js?id=AW-17645502549"
-  strategy="afterInteractive"
-/>
-
-<Script id="google-ads-tag" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'AW-17645502549');
-  `}
-</Script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M4MPXGRL');</script>
+<!-- End Google Tag Manager -->
         <Providers>
           <div className='min-h-screen'>{children}</div>
         </Providers>
